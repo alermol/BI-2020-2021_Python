65bdd9a1b59c7192795b2b9ed6153a0b3866ee32
Aleksey Ermolaev <ermol-2012@yandex.ru>
2020-09-13 00:31:11 +0300

    add empty line after inputs

--- calculator.py
+++ calculator.py
@@ -4,6 +4,7 @@
 first_number = float(input("Input first number: "))
 action = str(input("Input operator: "))
 second_number = float(input("Input second number: "))
+
 if action == "+":
     print(first_number + second_number)
     print("Done")
